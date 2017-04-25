979575e5e80619a73ce8832b71a05a4741a08284
Alexander Serbin <alexander.serbin@engagepoint.com>
2017-04-25 15:45:05 -0700

    #144141303 Configured Swagger for API

---
+++ cals-rest/src/main/java/gov/ca/cwds/cals/Constants.java
@@ -0,0 +1,9 @@
+package gov.ca.cwds.cals;
+
+public final class Constants {
+
+    private Constants() {}
+
+    public static final String PING = "ping";
+
+}
--- cals-rest/src/main/java/gov/ca/cwds/cals/rest/resources/PingResource.java
+++ cals-rest/src/main/java/gov/ca/cwds/cals/rest/resources/PingResource.java
@@ -1,16 +1,22 @@
 package gov.ca.cwds.cals.rest.resources;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/ping")
+import static gov.ca.cwds.cals.Constants.PING;
+
+@Api(value = PING, tags = PING)
+@Path(PING)
 @Produces(MediaType.TEXT_PLAIN)
 public class PingResource {
     
     @GET
+    @ApiOperation(value = "The simplest rest GET to check application is up and running", response = String.class)
     public String ping() {
         return "pong";
     }
