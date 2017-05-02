1934490b21babab8411aedca271af1490a6c4e09
Leonid Marushevskiy <leonid.marushevskiy@engagepoint.com>
2017-05-02 08:56:00 -0700

    #144331967: Facility DTO updated

--- src/main/java/gov/ca/cwds/cals/service/dto/FacilityDTO.java
+++ src/main/java/gov/ca/cwds/cals/service/dto/FacilityDTO.java
@@ -1,61 +1,159 @@
 package gov.ca.cwds.cals.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import gov.ca.cwds.rest.api.Request;
 import gov.ca.cwds.rest.api.Response;
+import gov.ca.cwds.rest.api.domain.DomainObject;
+import gov.ca.cwds.rest.validation.Date;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
+import java.util.Set;
 
 /**
- * A DTO for the Facility entity.
+ * {@link DomainObject} representing a facility.
+ *
+ * @author CWDS CALS API Team
  */
-public class FacilityDTO implements Serializable, Request, Response {
+public class FacilityDTO extends DomainObject implements Serializable, Request, Response {
 
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    @ApiModelProperty(required = true, readOnly = false, value = "Facility Number", example = "193600161")
     private Long id;
 
     @NotNull
+    @ApiModelProperty(required = true, readOnly = false, value = "Facility Type", example = "FAMILY DAY CARE")
     private String type;
 
     @NotNull
     @Size(max = 50)
+    @ApiModelProperty(required = true, readOnly = false, value = "Facility Name", example = "LITTLE ROADRUNNERS INFANT CARE")
     private String name;
 
     @NotNull
     @Size(max = 50)
+    @ApiModelProperty(required = false, readOnly = false, value = "Licensee Name", example = "BROWN, KIMBERLY")
     private String licenseeName;
 
+    @ApiModelProperty(required = false, readOnly = false, value = "Licensee Type", example = "A")
     private String licenseeType;
 
+    @ApiModelProperty(required = false, readOnly = false, value = "Assigned Worker", example = "PARKER, TONY")
     private String assignedWorker;
 
+    @ApiModelProperty(required = false, readOnly = false, value = "District Office", example = "MISSION VALLEY")
     private String districtOffice;
 
     @NotNull
+    @ApiModelProperty(required = false, readOnly = false, value = "License Number", example = "193600161")
     private Long licenseNumber;
 
+    @ApiModelProperty(required = false, readOnly = false, value = "License Status", example = "CLOSED, AGY INIT.")
     private String licenseStatus;
 
     @NotNull
+    @ApiModelProperty(required = false, readOnly = false, value = "Capacity", example = "10")
     private Integer capacity;
 
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT)
+    @gov.ca.cwds.rest.validation.Date(format = DATE_FORMAT, required = false)
+    @ApiModelProperty(required = false, readOnly = false, value = "yyyy-MM-dd", example = "2000-01-01")
     private LocalDate licenseEffectiveDate;
 
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT)
+    @gov.ca.cwds.rest.validation.Date(format = DATE_FORMAT, required = false)
+    @ApiModelProperty(required = false, readOnly = false, value = "yyyy-MM-dd", example = "2000-01-01")
     private LocalDate originalApplicationRecievedDate;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT)
+    @gov.ca.cwds.rest.validation.Date(format = DATE_FORMAT, required = false)
+    @ApiModelProperty(required = false, readOnly = false, value = "yyyy-MM-dd", example = "2000-01-01")
     private LocalDate lastVisitDate;
 
     @Size(max = 50)
+    @ApiModelProperty(required = false, readOnly = false, value = "Email Address", example = "name@mail.com")
     private String emailAddress;
 
+    @ApiModelProperty(required = false, readOnly = false, value = "Last Visit Reason", example = "Post Licensing")
     private String lastVisitReason;
 
     @NotNull
+    @ApiModelProperty(required = false, readOnly = false, value = "County", example = "SAN DIEGO")
     private String county;
+
+    @JsonProperty("address")
+    private Set<PersonAddressDTO> address;
+
+    @JsonProperty("address")
+    private Set<PersonPhoneDTO> phone;
+
+    /**
+     * Constructor
+     *
+     * @param id
+     * @param type
+     * @param name
+     * @param licenseeName
+     * @param licenseeType
+     * @param assignedWorker
+     * @param districtOffice
+     * @param licenseNumber
+     * @param licenseStatus
+     * @param capacity
+     * @param licenseEffectiveDate
+     * @param originalApplicationRecievedDate
+     * @param lastVisitDate
+     * @param emailAddress
+     * @param lastVisitReason
+     * @param county
+     * @param address
+     * @param phone
+     */
+    public FacilityDTO(@JsonProperty("id") Long id,
+                       @JsonProperty("type") String type,
+                       @JsonProperty("name") String name,
+                       @JsonProperty("licensee_name") String licenseeName,
+                       @JsonProperty("licensee_type") String licenseeType,
+                       @JsonProperty("assigned_worker") String assignedWorker,
+                       @JsonProperty("district_office") String districtOffice,
+                       @JsonProperty("license_number") Long licenseNumber,
+                       @JsonProperty("license_status") String licenseStatus,
+                       @JsonProperty("capacity") Integer capacity,
+                       @JsonProperty("license_effective_date") LocalDate licenseEffectiveDate,
+                       @JsonProperty("original_application_recieved_date") LocalDate originalApplicationRecievedDate,
+                       @JsonProperty("last_visit_date") LocalDate lastVisitDate,
+                       @JsonProperty("email_address") String emailAddress,
+                       @JsonProperty("last_visit_reason") String lastVisitReason,
+                       @JsonProperty("county") String county,
+                       @JsonProperty("addresses") Set<PersonAddressDTO> address,
+                       @JsonProperty("phones") Set<PersonPhoneDTO> phone) {
+        super();
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.licenseeName = licenseeName;
+        this.licenseeType = licenseeType;
+        this.assignedWorker = assignedWorker;
+        this.districtOffice = districtOffice;
+        this.licenseNumber = licenseNumber;
+        this.licenseStatus = licenseStatus;
+        this.capacity = capacity;
+        this.licenseEffectiveDate = licenseEffectiveDate;
+        this.originalApplicationRecievedDate = originalApplicationRecievedDate;
+        this.lastVisitDate = lastVisitDate;
+        this.emailAddress = emailAddress;
+        this.lastVisitReason = lastVisitReason;
+        this.county = county;
+    }
 
     public Long getId() {
         return id;
@@ -170,6 +268,22 @@ public class FacilityDTO implements Serializable, Request, Response {
         this.county = county;
     }
 
+    public Set<PersonAddressDTO> getAddress() {
+        return address;
+    }
+
+    public void setAddress(Set<PersonAddressDTO> address) {
+        this.address = address;
+    }
+
+    public Set<PersonPhoneDTO> getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Set<PersonPhoneDTO> phone) {
+        this.phone = phone;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -181,9 +295,7 @@ public class FacilityDTO implements Serializable, Request, Response {
 
         FacilityDTO facilityDTO = (FacilityDTO) o;
 
-        if ( ! Objects.equals(id, facilityDTO.id)) { return false; }
-
-        return true;
+        return Objects.equals(id, facilityDTO.id);
     }
 
     @Override
