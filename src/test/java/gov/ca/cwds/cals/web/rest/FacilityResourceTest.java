e0d0a51c03bc420b785358b658c297d7232a4527
Alexander Serbin <alexander.serbin@engagepoint.com>
2017-05-17 09:57:14 -0700

    added corner case test for facility by id API

--- src/test/java/gov/ca/cwds/cals/web/rest/FacilityResourceTest.java
+++ src/test/java/gov/ca/cwds/cals/web/rest/FacilityResourceTest.java
@@ -9,13 +9,16 @@ import org.junit.Test;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class FacilityResourceTest extends BaseCalsApiIntegrationTest {
 
     private static final String FACILITY_ID = "193600001";
+    private static final String WRONG_FACILITY_ID = "1";
 
     @BeforeClass
     public static void beforeClass() throws Exception {
@@ -33,5 +36,15 @@ public class FacilityResourceTest extends BaseCalsApiIntegrationTest {
         String fixture = fixture("fixtures/facility-by-id-response.json");
         assertThat(clientTestRule.getMapper().writeValueAsString(facilityDTO)).isEqualTo(fixture);
     }
+
+    @Test
+    public void testWrongFasilityId() throws Exception {
+        String restUrl = getUriString() + Constants.API.FACILITIES + "/" + WRONG_FACILITY_ID;
+        WebTarget target = clientTestRule.getClient().target(restUrl);
+        Invocation.Builder invocation = target.request(MediaType.APPLICATION_JSON);
+        Response response = invocation.get();
+        assertEquals(404, response.getStatus());
+    }
+
 
 }
