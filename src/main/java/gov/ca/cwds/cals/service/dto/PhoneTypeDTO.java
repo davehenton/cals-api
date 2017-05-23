fc6e376b58cb8b634317a9a39700b60ff02d2f05
Leonid Marushevskiy <leonid.marushevskiy@engagepoint.com>
2017-05-22 23:54:12 -0700

    SONAR: unused imports removed

--- src/main/java/gov/ca/cwds/cals/service/dto/PhoneTypeDTO.java
+++ src/main/java/gov/ca/cwds/cals/service/dto/PhoneTypeDTO.java
@@ -1,9 +1,5 @@
 package gov.ca.cwds.cals.service.dto;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.util.Objects;
-
 /**
  * A DTO for the PhoneType entity.
  */
