5e0468f4ecc132674b15b9f44abce8cc30feed0f
Leonid Marushevskiy <leonid.marushevskiy@engagepoint.com>
2017-06-13 22:26:35 -0700

    Sonar fixes

--- src/main/java/gov/ca/cwds/cals/service/dto/rs/ReplicatedFacilityDTO.java
+++ src/main/java/gov/ca/cwds/cals/service/dto/rs/ReplicatedFacilityDTO.java
@@ -2,11 +2,13 @@ package gov.ca.cwds.cals.service.dto.rs;
 
 import gov.ca.cwds.cals.service.dto.FacilityDTO;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import javax.validation.constraints.NotNull;
 
 /**
  * @author CWDS TPT-2
  */
+@SuppressWarnings("squid:S3437") //LocalDate is serializable
 public class ReplicatedFacilityDTO extends FacilityDTO {
   private static final long serialVersionUID = 1L;
 
@@ -30,5 +32,24 @@ public class ReplicatedFacilityDTO extends FacilityDTO {
 
   public void setReplicationDate(LocalDateTime replicationDate) {
     this.replicationDate = replicationDate;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    ReplicatedFacilityDTO replicatedFacilityDTO = (ReplicatedFacilityDTO) o;
+
+    return Objects.equals(getId(), replicatedFacilityDTO.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(getId());
   }
 }
