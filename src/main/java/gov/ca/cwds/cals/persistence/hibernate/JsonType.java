93f7d02bc6d7acb5de12f12ec093f96ba15814d5
Vladimir Petrusha <vladimr@petrusha.net>
2017-06-26 10:01:36 -0700

    Sonar issues fix

--- src/main/java/gov/ca/cwds/cals/persistence/hibernate/JsonType.java
+++ src/main/java/gov/ca/cwds/cals/persistence/hibernate/JsonType.java
@@ -3,6 +3,7 @@ package gov.ca.cwds.cals.persistence.hibernate;
 import static java.sql.Types.JAVA_OBJECT;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -10,6 +11,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -41,41 +43,53 @@ public abstract class JsonType implements UserType {
   }
 
   @Override
-  public Object nullSafeGet(ResultSet rs, String[] names, SharedSessionContractImplementor session,
-      Object owner) throws SQLException {
-    final String cellContent = rs.getString(names[0]);
-    if (cellContent == null) {
-      return null;
-    }
+  public Object nullSafeGet(
+      ResultSet rs, String[] names, SharedSessionContractImplementor session, Object owner)
+      throws SQLException {
     try {
-      return mapper.readValue(cellContent.getBytes("UTF-8"), returnedClass());
+      final String cellContent = rs.getString(names[0]);
+      if (cellContent == null) {
+        return null;
+      }
+      return mapper.readValue(cellContent.getBytes(StandardCharsets.UTF_8), returnedClass());
+    } catch (final SQLException sqle) {
+      throw sqle;
     } catch (final Exception ex) {
       throw new ConvertingException(
-          String.format("Failed to convert String to %s: %s", returnedClass().getSimpleName(),
-              ex.getMessage()), ex);
+          String.format(
+              "Failed to convert String to %s: %s",
+              returnedClass().getSimpleName(), ex.getMessage()),
+          ex);
     }
   }
 
   @Override
-  public void nullSafeSet(PreparedStatement st, Object value, int index,
-      SharedSessionContractImplementor session) throws SQLException {
-    if (value == null) {
-      st.setNull(index, Types.OTHER);
-      return;
-    }
+  public void nullSafeSet(
+      PreparedStatement st, Object value, int index, SharedSessionContractImplementor session)
+      throws SQLException {
     try {
+      if (value == null) {
+        st.setNull(index, Types.OTHER);
+        return;
+      }
+
       final StringWriter stringWriter = new StringWriter();
       mapper.writeValue(stringWriter, value);
       stringWriter.flush();
       st.setObject(index, stringWriter.toString(), Types.OTHER);
+    } catch (final SQLException sqle) {
+      throw sqle;
     } catch (final Exception ex) {
       throw new ConvertingException(
-          String.format("Failed to convert %s to String: %s", returnedClass().getSimpleName(),
-              ex.getMessage()), ex);
+          String.format(
+              "Failed to convert %s to String: %s",
+              returnedClass().getSimpleName(), ex.getMessage()),
+          ex);
     }
   }
 
   @Override
+  @SuppressFBWarnings("OBJECT_DESERIALIZATION") // There is no external objects
   public Object deepCopy(Object value) {
     try {
       ByteArrayOutputStream bos = new ByteArrayOutputStream();
