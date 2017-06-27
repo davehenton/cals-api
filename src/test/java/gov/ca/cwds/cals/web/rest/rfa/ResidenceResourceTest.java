c85362c855ffd1753b8e861b04d8a9c014ac001a
oleg.korneychuk <oleg.korneychuk@engagepoint.com>
2017-06-27 15:40:47 -0700

    147207485 Fix boolean properties duplication

---
+++ src/test/java/gov/ca/cwds/cals/web/rest/rfa/ResidenceResourceTest.java
@@ -0,0 +1,76 @@
+package gov.ca.cwds.cals.web.rest.rfa;
+
+import gov.ca.cwds.cals.BaseCalsApiIntegrationTest;
+import gov.ca.cwds.cals.persistence.model.calsns.rfa.RFA1aForm;
+import gov.ca.cwds.cals.persistence.model.calsns.rfa.Residence;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+
+import static gov.ca.cwds.cals.Constants.API.RESIDENCE;
+import static gov.ca.cwds.cals.Constants.API.RFA_1A_FORMS;
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * @author CWDS CALS API Team
+ */
+public class ResidenceResourceTest extends BaseCalsApiIntegrationTest {
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        setUpCalsns();
+    }
+
+    @Test
+    public void testGetResidenceApplicationNotFound() throws Exception {
+        WebTarget target = clientTestRule.target(RFA_1A_FORMS + "/-1/" + RESIDENCE);
+        Invocation.Builder invocation = target.request(MediaType.APPLICATION_JSON);
+        try {
+            invocation.get(Residence.class);
+            fail();
+        } catch (NotFoundException e) {
+            assertEquals(404, e.getResponse().getStatus());
+            assertEquals("HTTP 404 Not Found", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testPutResidenceApplicationNotFound() throws Exception {
+        WebTarget target = clientTestRule.target(RFA_1A_FORMS + "/-1/" + RESIDENCE);
+        Invocation.Builder invocation = target.request(MediaType.APPLICATION_JSON);
+        String fixture = fixture("fixtures/rfa/rfa-1a-residence-request.json");
+        Residence residence = clientTestRule.getMapper().readValue(fixture, Residence.class);
+        try {
+            invocation.put(Entity.entity(residence, MediaType.APPLICATION_JSON), Residence.class);
+            fail();
+        } catch (NotFoundException e) {
+            assertEquals(404, e.getResponse().getStatus());
+            assertEquals("HTTP 404 Not Found", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testPutAndGetResidence() throws Exception {
+        WebTarget formTarget = clientTestRule.target(RFA_1A_FORMS);
+        Invocation.Builder formInvocation = formTarget.request(MediaType.APPLICATION_JSON);
+        RFA1aForm form = formInvocation.post(Entity.entity(null, MediaType.APPLICATION_JSON), RFA1aForm.class);
+
+        WebTarget residenceTarget = clientTestRule.target(RFA_1A_FORMS + "/" + form.getId() + "/" + RESIDENCE);
+        Invocation.Builder residenceInvocation = residenceTarget.request(MediaType.APPLICATION_JSON);
+        String fixture = fixture("fixtures/rfa/rfa-1a-residence-request.json");
+        Residence residenceRequest = clientTestRule.getMapper().readValue(fixture, Residence.class);
+        Residence residenceResponse = residenceInvocation.put(Entity.entity(residenceRequest, MediaType.APPLICATION_JSON), Residence.class);
+
+        String expectedFixture = fixture("fixtures/rfa/rfa-1a-residence-response.json");
+        Residence expectedResidenceResponse = clientTestRule.getMapper().readValue(expectedFixture, Residence.class);
+        assertEquals(expectedResidenceResponse, residenceResponse);
+    }
+}
