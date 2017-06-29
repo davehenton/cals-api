dca89f211d3c21441eec395eb812950691a1c6da
Alexander Serbin <alexander.serbin@engagepoint.com>
2017-06-29 12:23:58 -0700

    reworked internal entities approach

---
+++ src/main/java/gov/ca/cwds/cals/service/rfa/AbstractInternalEntityService.java
@@ -0,0 +1,55 @@
+package gov.ca.cwds.cals.service.rfa;
+
+import static gov.ca.cwds.cals.Constants.API.SYSTEM_USER_ID;
+import static gov.ca.cwds.cals.web.rest.exception.CalsExceptionInfo.RFA_1A_APPLICATION_NOT_FOUND_BY_ID;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+
+import gov.ca.cwds.cals.persistence.dao.calsns.RFA1aFormsDao;
+import gov.ca.cwds.cals.persistence.model.calsns.rfa.RFA1aForm;
+import gov.ca.cwds.cals.service.CrudServiceAdapter;
+import gov.ca.cwds.cals.service.dto.BaseDTO;
+import gov.ca.cwds.cals.service.rfa.configuration.InternalEntityConfiguration;
+import gov.ca.cwds.cals.web.rest.exception.UserFriendlyException;
+import gov.ca.cwds.rest.api.Request;
+import gov.ca.cwds.rest.api.Response;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author CWDS CALS API Team.
+ */
+public abstract class AbstractInternalEntityService<T extends BaseDTO> extends CrudServiceAdapter {
+
+  private final RFA1aFormsDao applicationDao;
+  private InternalEntityConfiguration<T> configuration;
+
+  public AbstractInternalEntityService(RFA1aFormsDao applicationDao,
+      InternalEntityConfiguration<T> configuration) {
+    this.applicationDao = applicationDao;
+    this.configuration = configuration;
+  }
+
+  @Override
+  public Response find(Serializable applicationId) {
+    RFA1aForm form = applicationDao.find(applicationId);
+    if (form == null) {
+      throw new UserFriendlyException(RFA_1A_APPLICATION_NOT_FOUND_BY_ID, NOT_FOUND);
+    }
+    return configuration.getEntityFromTheForm(form);
+  }
+
+  @Override
+  public Response update(Serializable applicationId, Request request) {
+    RFA1aForm form = applicationDao.find(applicationId);
+    if (form == null) {
+      throw new UserFriendlyException(RFA_1A_APPLICATION_NOT_FOUND_BY_ID, NOT_FOUND);
+    }
+    form.setUpdateDateTime(LocalDateTime.now());
+    form.setUpdateUserId(SYSTEM_USER_ID);
+
+    configuration.putEntityToTheForm(form, request);
+
+    RFA1aForm updatedForm = applicationDao.update(form);
+    return configuration.getEntityFromTheForm(updatedForm);
+  }
+}
---
+++ src/main/java/gov/ca/cwds/cals/service/rfa/configuration/InternalEntityConfiguration.java
@@ -0,0 +1,41 @@
+package gov.ca.cwds.cals.service.rfa.configuration;
+
+import gov.ca.cwds.cals.persistence.model.calsns.rfa.RFA1aForm;
+import gov.ca.cwds.cals.service.dto.BaseDTO;
+import gov.ca.cwds.rest.api.Request;
+import gov.ca.cwds.rest.api.Response;
+
+/**
+ * @author CWDS CALS API Team
+ */
+
+public abstract class InternalEntityConfiguration<T extends BaseDTO> {
+
+  private Class<T> entityClass;
+
+  public InternalEntityConfiguration(Class<T> entityClass) {
+    this.entityClass = entityClass;
+  }
+
+  public Class<T> getEntityClass() {
+    return entityClass;
+  }
+
+  protected abstract T retrieveEntityFromTheForm(RFA1aForm form);
+
+  public Response getEntityFromTheForm(RFA1aForm form) {
+    T entity = retrieveEntityFromTheForm(form);
+    return (Response) entity;
+  }
+
+  public void putEntityToTheForm(RFA1aForm form, Request request) {
+    if (!(request.getClass().isAssignableFrom(entityClass))) {
+      throw new IllegalStateException(
+          "Unexpected request class type. Expected is " + entityClass.getSimpleName());
+    }
+    saveEntityToTheForm(form, (T) request);
+  }
+
+  protected abstract void saveEntityToTheForm(RFA1aForm form, T entity);
+
+}
