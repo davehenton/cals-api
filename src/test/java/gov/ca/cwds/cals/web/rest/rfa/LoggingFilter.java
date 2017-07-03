1013c78526c8a064807a58eeeb1836947ee43393
Leonid Marushevskiy <leonid.marushevskiy@engagepoint.com>
2017-07-03 11:22:02 -0700

    #147980115: RFA 1B contract

--- src/test/java/gov/ca/cwds/cals/web/rest/rfa/LoggingFilter.java
+++ src/test/java/gov/ca/cwds/cals/web/rest/rfa/LoggingFilter.java
@@ -2,6 +2,7 @@ package gov.ca.cwds.cals.web.rest.rfa;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -25,7 +26,7 @@ public class LoggingFilter implements ClientRequestFilter, ClientResponseFilter 
   @Override
   public void filter(ClientRequestContext requestContext) throws IOException {
     ObjectMapper mapper = new ObjectMapper();
-    mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    mapper.registerModule(new JavaTimeModule());
 
     LOG.info(System.getProperty("line.separator") + "!!! Test Request" + System.getProperty("line.separator") + "!!! URL: {}" + System.getProperty("line.separator") + "!!! Method: {}" + System.getProperty("line.separator") + "!!! Body: {}", requestContext.getUri(),
         requestContext.getMethod(),
