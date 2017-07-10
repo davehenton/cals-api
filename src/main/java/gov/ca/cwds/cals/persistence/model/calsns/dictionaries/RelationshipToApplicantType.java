caa55ff1a8c39e8d7407d0df7efded4992d81578
oleg.korneychuk <oleg.korneychuk@engagepoint.com>
2017-07-10 10:41:39 -0700

    148379667 Add Relationship to Applicant type

--- src/main/java/gov/ca/cwds/cals/persistence/model/calsns/dictionaries/RelationshipToApplicantType.java
+++ src/main/java/gov/ca/cwds/cals/persistence/model/calsns/dictionaries/RelationshipToApplicantType.java
@@ -1,10 +1,20 @@
 package gov.ca.cwds.cals.persistence.model.calsns.dictionaries;
 
+import org.hibernate.annotations.NamedQuery;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author CWDS CALS API Team
  */
+@NamedQuery(name = RelationshipToApplicantType.NAMED_QUERY_FIND_ALL, query = "FROM RelationshipToApplicantType ORDER BY id ASC")
+@Entity
+@Table(name = "relationship_to_applicant_type")
 public class RelationshipToApplicantType extends BaseDictionary {
 
-  private static final long serialVersionUID = -4887203144793399786L;
+  private static final long serialVersionUID = 1L;
 
+  public static final String NAMED_QUERY_FIND_ALL =
+      NAMED_QUERY_PREFIX + ".RelationshipToApplicantType" + NAMED_QUERY_FIND_ALL_SUFFIX;
 }
