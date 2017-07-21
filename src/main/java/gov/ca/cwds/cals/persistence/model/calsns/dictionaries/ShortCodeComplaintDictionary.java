7f259b8ad2a7fa17521b78543c861c488a0f9ce5
Vladimir Petrusha <vladimr@petrusha.net>
2017-07-21 13:40:35 -0700

    #148639089 Add ShortCodeComplaintDictionary class Update fixtures

---
+++ src/main/java/gov/ca/cwds/cals/persistence/model/calsns/dictionaries/ShortCodeComplaintDictionary.java
@@ -0,0 +1,40 @@
+package gov.ca.cwds.cals.persistence.model.calsns.dictionaries;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.Column;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ * @author CWDS CALS API Team
+ */
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class ShortCodeComplaintDictionary extends BaseDictionary {
+  private static final long serialVersionUID = 8931766854976564993L;
+
+  @JsonIgnore
+  @Column(name = "cws_short_code", length = 2)
+  private String cwsShortCode;
+
+  public String getCwsShortCode() {
+    return cwsShortCode;
+  }
+
+  public void setCwsShortCode(String cwsShortCode) {
+    this.cwsShortCode = cwsShortCode;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    return EqualsBuilder.reflectionEquals(this, o, "cwsShortCode");
+  }
+
+  @Override
+  public int hashCode() {
+    return HashCodeBuilder.reflectionHashCode(this, "cwsShortCode");
+  }
+}
