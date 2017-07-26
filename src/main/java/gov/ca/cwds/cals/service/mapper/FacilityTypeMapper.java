007e2e9bc39f32ffeb451fd6a4ba71d1b8edf56c
oleg.korneychuk <oleg.korneychuk@engagepoint.com>
2017-07-26 10:18:37 -0700

    Remove deprecated stuff

--- src/main/java/gov/ca/cwds/cals/service/mapper/FacilityTypeMapper.java
+++ src/main/java/gov/ca/cwds/cals/service/mapper/FacilityTypeMapper.java
@@ -5,19 +5,11 @@ import gov.ca.cwds.cals.service.dto.FacilityTypeDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import java.util.List;
-
 /**
  * @author CWDS CALS API Team
  */
 @Mapper(uses = TrailingSpacesRemovalPostMappingProcessor.class)
 public interface FacilityTypeMapper {
-
-    List<FacilityTypeDTO> toFacilityTypeDTO(List<gov.ca.cwds.cals.persistence.model.lisfas.FacilityType> facilityTypes);
-
-    @Mapping(target = "code", source = "tblFacTypeCode")
-    @Mapping(target = "description", source = "tblFacTypeDesc")
-    FacilityTypeDTO toFacilityTypeDTO(gov.ca.cwds.cals.persistence.model.lisfas.FacilityType facilityType);
 
     @Mapping(target = "code", source = "tblFacTypeCode")
     @Mapping(target = "description", source = "tblFacTypeDesc")
