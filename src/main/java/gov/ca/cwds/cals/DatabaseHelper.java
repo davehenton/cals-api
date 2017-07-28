08303c8b31b6d337f1ba2a5ffb5b9c5c70d2fe1b
Vladimir Petrusha <vladimr@petrusha.net>
2017-07-27 17:11:08 -0700

    #149377955 DataBase upgrade implementation

---
+++ src/main/java/gov/ca/cwds/cals/DatabaseHelper.java
@@ -0,0 +1,58 @@
+package gov.ca.cwds.cals;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+/**
+ * @author CWDS CALS API Team
+ */
+public class DatabaseHelper {
+
+    private Database database;
+    private String url;
+    private String user;
+    private String password;
+
+    public DatabaseHelper(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public void runScript(String script) throws LiquibaseException {
+        try {
+            new Liquibase(script, new ClassLoaderResourceAccessor(), getDatabase())
+                .update((String) null);
+        } catch (Exception e) {
+            throw new LiquibaseException(e);
+        }
+    }
+
+    public void runScript(String script, String schema) throws LiquibaseException {
+        try {
+            String defaultSchema = getDatabase().getDefaultSchemaName();
+            getDatabase().setDefaultSchemaName(schema);
+            runScript(script);
+            getDatabase().setDefaultSchemaName(defaultSchema);
+        } catch (Exception e) {
+            throw new LiquibaseException(e);
+        }
+    }
+
+    private Database getDatabase() throws SQLException, DatabaseException {
+        if (database == null) {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+        }
+
+        return database;
+    }
+}
