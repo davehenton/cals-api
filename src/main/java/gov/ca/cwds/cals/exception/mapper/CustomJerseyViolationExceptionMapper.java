b941c2f5f4f461eb43095daa14031ce939a2a0c3
Alexander Serbin <alexander.serbin@engagepoint.com>
2017-08-04 14:19:04 -0700

    fixed 2 bugs: dropwizard valiadtion messages caching and nullpointer for empty applicant

--- src/main/java/gov/ca/cwds/cals/exception/mapper/CustomJerseyViolationExceptionMapper.java
+++ src/main/java/gov/ca/cwds/cals/exception/mapper/CustomJerseyViolationExceptionMapper.java
@@ -1,12 +1,14 @@
 package gov.ca.cwds.cals.exception.mapper;
 
+
 import gov.ca.cwds.cals.exception.BaseExceptionResponse;
 import gov.ca.cwds.cals.exception.ExceptionType;
 import io.dropwizard.jersey.validation.JerseyViolationException;
-import io.dropwizard.jersey.validation.JerseyViolationExceptionMapper;
-import io.dropwizard.jersey.validation.ValidationErrorMessage;
 import java.util.List;
+import java.util.stream.Collectors;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 /**
@@ -14,18 +16,26 @@ import javax.ws.rs.ext.Provider;
  */
 
 @Provider
-public class CustomJerseyViolationExceptionMapper extends JerseyViolationExceptionMapper {
+public class CustomJerseyViolationExceptionMapper implements
+    ExceptionMapper<JerseyViolationException> {
 
   @Override
   public Response toResponse(final JerseyViolationException exception) {
-    Response response = super.toResponse(exception);
     BaseExceptionResponse constraintViolationsResponse = new BaseExceptionResponse();
     constraintViolationsResponse.setExceptionType(ExceptionType.VALIDATION_ERROR);
     constraintViolationsResponse.setCode("?");
-    List<String> userMessages = ((ValidationErrorMessage) response.getEntity()).getErrors();
+    List<String> userMessages =
+        exception
+            .getConstraintViolations()
+            .stream()
+            .map(v -> CustomConstraintMessage.getMessage(v, exception.getInvocable()))
+            .collect(Collectors.toList());
     constraintViolationsResponse.setUserMessages(userMessages);
-    return Response.status(response.getStatus())
-        .type(response.getMediaType())
+    int status =
+        CustomConstraintMessage.determineStatus(
+            exception.getConstraintViolations(), exception.getInvocable());
+    return Response.status(status)
+        .type(MediaType.APPLICATION_JSON_TYPE)
         .entity(constraintViolationsResponse)
         .build();
   }
