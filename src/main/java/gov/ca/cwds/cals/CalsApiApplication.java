ac868995c180a47f7eff9432a71045ceb603e2c6
oleg.korneychuk <oleg.korneychuk@engagepoint.com>
2017-08-09 11:28:43 -0700

    Update health check queries

--- src/main/java/gov/ca/cwds/cals/CalsApiApplication.java
+++ src/main/java/gov/ca/cwds/cals/CalsApiApplication.java
@@ -7,14 +7,12 @@ import gov.ca.cwds.cals.exception.CustomExceptionMapperBinder;
 import gov.ca.cwds.cals.exception.mapper.BusinessValidationExceptionMapper;
 import gov.ca.cwds.cals.exception.mapper.ExpectedExceptionMapperImpl;
 import gov.ca.cwds.cals.exception.mapper.UnexpectedExceptionMapperImpl;
-import gov.ca.cwds.cals.health.DataSourceHealthCheck;
 import gov.ca.cwds.cals.inject.ApplicationModule;
 import gov.ca.cwds.cals.inject.InjectorHolder;
 import gov.ca.cwds.rest.BaseApiApplication;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import java.util.Map;
 import org.glassfish.jersey.linking.DeclarativeLinkingFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,37 +43,34 @@ public class CalsApiApplication extends BaseApiApplication<CalsApiConfiguration>
     environment.jersey().register(BusinessValidationExceptionMapper.class);
     environment.jersey().register(new CustomExceptionMapperBinder(true));
 
-    HealthCheckRegistry healthCheckRegistry = environment.healthChecks();
-    healthCheckRegistry.register(
-        "nsDataSource", new DataSourceHealthCheck(configuration.getNsDataSourceFactory()));
-    healthCheckRegistry.register(
-        "cmsDataSource", new DataSourceHealthCheck(configuration.getCmsDataSourceFactory()));
-    healthCheckRegistry.register(
-        "lisDataSource", new DataSourceHealthCheck(configuration.getLisDataSourceFactory()));
-    healthCheckRegistry.register(
-        "calsnsDataSource", new DataSourceHealthCheck(configuration.getCalsnsDataSourceFactory()));
+    if (configuration.isUpgradeDbOnStart()) {
+      upgardeCalsNsDB(configuration);
+    }
+
     environment
         .jersey()
         .getResourceConfig()
         .packages(getClass().getPackage().getName())
         .register(DeclarativeLinkingFeature.class);
 
-    runHealthChecks(environment);
+    runDataSourceHealthChecks(environment);
 
     // Providing access to the guice injector from external classes such as custom validators
     InjectorHolder.INSTANCE.setInjector(this.guiceBundle.getInjector());
-
-    if (configuration.isUpgradeDbOnStart()) {
-      upgardeCalsNsDB(configuration);
-    }
   }
 
-  private void runHealthChecks(Environment environment) {
-    for (Map.Entry<String, HealthCheck.Result> entry :
-        environment.healthChecks().runHealthChecks().entrySet()) {
-      if (!entry.getValue().isHealthy()) {
-        LOG.error("Fail - {}: {}", entry.getKey(), entry.getValue().getMessage());
-      }
+  private void runDataSourceHealthChecks(Environment environment) {
+    HealthCheckRegistry healthCheckRegistry = environment.healthChecks();
+    doHealthCheck(healthCheckRegistry, Constants.UnitOfWork.CALSNS);
+    doHealthCheck(healthCheckRegistry, Constants.UnitOfWork.FAS);
+    doHealthCheck(healthCheckRegistry, Constants.UnitOfWork.LIS);
+    doHealthCheck(healthCheckRegistry, Constants.UnitOfWork.CMS);
+  }
+
+  private void doHealthCheck(HealthCheckRegistry healthCheckRegistry, String key) {
+    HealthCheck.Result result = healthCheckRegistry.runHealthCheck(key);
+    if (!result.isHealthy()) {
+      LOG.error("Fail - {}: {}", key, result.getMessage());
     }
   }
 
