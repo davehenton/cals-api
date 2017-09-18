143d965eb2a2d818bc591a1c467d8c34aafda2e3
Oleksander Kuznetsov <oleksander.kuznetsov@engagepoint.com>
2017-09-18 11:19:33 -0700

    Update cals-api entrypoint to run newrelic agent

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,9 @@ fi
 
 echo "config file: $CALS_API_CONFIG"
 
-java -jar cals-api.jar server $CALS_API_CONFIG
+
+if [ -f /opt/newrelic/newrelic.yml ]; then
+    java -javaagent:/opt/newrelic/newrelic.jar  ${JAVA_OPTS} -jar cals-api.jar server $CALS_API_CONFIG
+else
+    java  ${JAVA_OPTS} -jar cals-api.jar server $CALS_API_CONFIG
+fi
