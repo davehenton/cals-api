62a110c99d5b89ffce55c2da8340b1428e560301
Sergii Redchuk <serge.redchuk@engagepoint.com>
2017-10-03 17:30:41 -0700

    DMS PDF: Develop generation of RFA 1B PDF

--- src/main/resources/dms/rfa1b-form/RFA-1B-form.groovy
+++ src/main/resources/dms/rfa1b-form/RFA-1B-form.groovy
@@ -1,66 +1,40 @@
 import org.apache.commons.lang3.StringUtils
 
-def getChildName = {
-    StringUtils.trimToNull(StringUtils.joinWith(" ",
-        it?.first_name,
-        it?.middle_name,
-        it?.last_name
-    ));
-}
+def yesValue = { it ? "Yes" : "Off" }
+def noValue = { it ? "Off" : "Yes" }
 
-def getSchoolName = {
-    StringUtils.trimToNull(StringUtils.joinWith(" ",
-        it?.school_grade?.value,
-        it?.school_name,
-        it?.school_address?.street_address,
-        it?.school_address?.city,
-        it?.school_address?.state?.value,
-        it?.school_address?.zip
-    ));
-}
+def otherStatesOfLiving = jsonMap.other_states_of_living?.value
 
-def getRelationshipToApplicants = {
-    String[] names = it? it.relationship_to_applicants*.relationship_to_applicant?.value : [];
-    StringUtils.joinWith(", ", names);
+def getFullName = {
+    StringUtils.trimToNull(StringUtils.joinWith(" ",
+            it?.applicant_name_prefix?.value,
+            it?.applicant_first_name,
+            it?.applicant_middle_name,
+            it?.applicant_last_name,
+            it?.applicant_name_suffix?.value
+    ))
 }
 
 [
-  "COUNTY": jsonMap.application_county.value,
+  'Have you ever been convicted of a crime in another state, federal court, military, or a jurisdiction outside of the U.S. ?  yes': yesValue(jsonMap.convicted_in_another_state),
+  'Have you ever been convicted of a crime in California check yesHave you ever been convicted of a crime in another state, federal court, military, or a jurisdiction outside of the U.S. ?  no': noValue(jsonMap.convicted_in_another_state),
 
-  "NAME OF CHILDRow1": getChildName(jsonMap.identified_children[0]),
-  "DATE OF BIRTH OF CHILDRow1": jsonMap.identified_children[0]?.date_of_birth,
-  "GENDERRow1": jsonMap.identified_children[0]?.gender?.value,
-  "COUNTY OF JURISDICTIONRow1": jsonMap.identified_children[0]?.county_of_jurisdiction?.value,
-  "DATE OF PLACEMENTRow1": jsonMap.identified_children[0]?.date_of_placement,
-  "RELATIONSHIP TO APPLICANTSRow1": getRelationshipToApplicants(jsonMap.identified_children[0]),
-  "EDUCATION GRADE NAME  ADDRESS OF SCHOOLRow1": getSchoolName(jsonMap.identified_children[0]),
+  'P1_CHK_Have_you_ever_been_convicted_of_a_crime_in_california?_yes': yesValue(jsonMap.convicted_in_california),
+  'P1_CHK_Have_you_ever_been_convicted_of_a_crime_in_california_check_no': noValue(jsonMap.convicted_in_california),
 
-  "NAME OF CHILDRow2": getChildName(jsonMap.identified_children[1]),
-  "DATE OF BIRTH OF CHILDRow2": jsonMap.identified_children[1]?.date_of_birth,
-  "GENDERRow2": jsonMap.identified_children[1]?.gender?.value,
-  "COUNTY OF JURISDICTIONRow2": jsonMap.identified_children[1]?.county_of_jurisdiction?.value,
-  "DATE OF PLACEMENTRow2": jsonMap.identified_children[1]?.date_of_placement,
-  "RELATIONSHIP TO APPLICANTSRow2": getRelationshipToApplicants(jsonMap.identified_children[1]),
-  "EDUCATION GRADE NAME  ADDRESS OF SCHOOLRow2": getSchoolName(jsonMap.identified_children[1]),
+  'P1_CHK_Have_you_ever_been_convicted_of_a_crime_in_california_check_yeshave_you_ever_been_arrested_for_a_crime_against_a_child_or_for_spousal/cohabitant_abuse?_yes': yesValue(jsonMap.arrested_for_crime),
+  'P1_CHK_Have_you_ever_been_convicted_of_a_crime_in_california_check_yeshave_you_ever_been_convicted_of_a_crime_in_california_check_yeshave_you_ever_been_arrested_for_a_crime_against_a_child_or_for_spousal/cohabitant_abuse?_no': noValue(jsonMap.arrested_for_crime),
 
-  "NAME OF CHILDRow3": getChildName(jsonMap.identified_children[2]),
-  "DATE OF BIRTH OF CHILDRow3": jsonMap.identified_children[2]?.date_of_birth,
-  "GENDERRow3": jsonMap.identified_children[2]?.gender?.value,
-  "COUNTY OF JURISDICTIONRow3": jsonMap.identified_children[2]?.county_of_jurisdiction?.value,
-  "DATE OF PLACEMENTRow3": jsonMap.identified_children[2]?.date_of_placement,
-  "RELATIONSHIP TO APPLICANTSRow3": getRelationshipToApplicants(jsonMap.identified_children[2]),
-  "EDUCATION GRADE NAME  ADDRESS OF SCHOOLRow3": getSchoolName(jsonMap.identified_children[2]),
+  'P1_CHK_Have_you_lived_in_a_state_other_than_california_within_the_last_five_years?__yes': yesValue(jsonMap.lived_in_other_state),
+  'P1_CHK_Have_you_lived_in_a_state_other_than_california_within_the_last_five_years?__no': noValue(jsonMap.lived_in_other_state),
+  'P1_If_yes_line_1': otherStatesOfLiving ? otherStatesOfLiving[0] : '',
+  'P1_If_yes_line_2': otherStatesOfLiving && otherStatesOfLiving.size() > 1 ? otherStatesOfLiving[1..-1].join(', ') : '',
 
-  "NAME OF CHILDRow4": getChildName(jsonMap.identified_children[3]),
-  "DATE OF BIRTH OF CHILDRow4": jsonMap.identified_children[3]?.date_of_birth,
-  "GENDERRow4": jsonMap.identified_children[3]?.gender?.value,
-  "COUNTY OF JURISDICTIONRow4": jsonMap.identified_children[3]?.county_of_jurisdiction?.value,
-  "DATE OF PLACEMENTRow4": jsonMap.identified_children[3]?.date_of_placement,
-  "RELATIONSHIP TO APPLICANTSRow4": getRelationshipToApplicants(jsonMap.identified_children[3]),
-  "EDUCATION GRADE NAME  ADDRESS OF SCHOOLRow4": getSchoolName(jsonMap.identified_children[3]),
+  'P1_County': jsonMap.application_county?.value,
+  'P1_Date_of_birth': jsonMap.date_of_birth,
+  'P1_Drivers_license_number/state': jsonMap.driver_license +'/'+ jsonMap.driver_license_state?.id,
 
-  "Yes-Child has been identified": jsonMap.child_identified ? "Yes" : "Off",
-  "No-Child has been identified": jsonMap.child_identified ? "Off" : "Yes",
-  "Yes-Child currently in home": jsonMap.child_in_home ? "Yes" : "Off",
-  "No-Child currently in home": jsonMap.child_in_home ? "Off" : "Yes"
+  'P1_Social_security_number_see_privacy_statement': jsonMap.ssn,
+
+  'P1_Your_full_name_print_clearly': getFullName(jsonMap)
 ]
