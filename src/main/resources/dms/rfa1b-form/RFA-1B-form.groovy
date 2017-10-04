46ddfcde00d6f88ad6e9bd8c11bba1547e04d487
Sergii Redchuk <serge.redchuk@engagepoint.com>
2017-10-04 10:55:03 -0700

    DMS PDF: Develop generation of RFA 1B PDF

--- src/main/resources/dms/rfa1b-form/RFA-1B-form.groovy
+++ src/main/resources/dms/rfa1b-form/RFA-1B-form.groovy
@@ -19,7 +19,7 @@ def getAddressStreetCityStateZip = {
     [
             it?.street_address,
             it?.city,
-            it?.state?.value,
+            it?.state?.id,
             it?.zip
     ]
 }
@@ -34,36 +34,34 @@ def dateIsoToUs = {
 }
 
 [
-  'Have you ever been convicted of a crime in another state, federal court, military, or a jurisdiction outside of the U.S. ?  yes': yesValue(jsonMap.convicted_in_another_state),
-  'Have you ever been convicted of a crime in California check yesHave you ever been convicted of a crime in another state, federal court, military, or a jurisdiction outside of the U.S. ?  no': noValue(jsonMap.convicted_in_another_state),
+  'Have you ever been convicted of a crime in another state, federal court, military, or a jurisdiction outside of the U': yesValue(jsonMap.convicted_in_another_state),
+  'Have you ever been convicted of a crime in California check yesHave you ever been convicted of a crime in another state, federal court, military, or a jurisdiction outside of the U': noValue(jsonMap.convicted_in_another_state),
 
-  'P1_CHK_Have_you_ever_been_convicted_of_a_crime_in_california?_yes': yesValue(jsonMap.convicted_in_california),
-  'P1_CHK_Have_you_ever_been_convicted_of_a_crime_in_california_check_no': noValue(jsonMap.convicted_in_california),
+  'Have you ever been convicted of a crime in California? Yes': yesValue(jsonMap.convicted_in_california),
+  'Have you ever been convicted of a crime in California check no': noValue(jsonMap.convicted_in_california),
 
-  'P1_CHK_Have_you_ever_been_convicted_of_a_crime_in_california_check_yeshave_you_ever_been_arrested_for_a_crime_against_a_child_or_for_spousal/cohabitant_abuse?_yes': yesValue(jsonMap.arrested_for_crime),
-  'P1_CHK_Have_you_ever_been_convicted_of_a_crime_in_california_check_yeshave_you_ever_been_convicted_of_a_crime_in_california_check_yeshave_you_ever_been_arrested_for_a_crime_against_a_child_or_for_spousal/cohabitant_abuse?_no': noValue(jsonMap.arrested_for_crime),
+  'Have you ever been convicted of a crime in California check yesHave you ever been arrested for a crime against a child or for spousal/cohabitant abuse? yes': yesValue(jsonMap.arrested_for_crime),
+  'Have you ever been convicted of a crime in California check yesHave you ever been convicted of a crime in California check yesHave you ever been arrested for a crime against a child or for spousal/cohabitant abuse? no': noValue(jsonMap.arrested_for_crime),
 
-  'P1_CHK_Have_you_lived_in_a_state_other_than_california_within_the_last_five_years?__yes': yesValue(jsonMap.lived_in_other_state),
-  'P1_CHK_Have_you_lived_in_a_state_other_than_california_within_the_last_five_years?__no': noValue(jsonMap.lived_in_other_state),
-  'P1_If_yes_line_1': otherStatesOfLiving ? otherStatesOfLiving[0] : '',
-  'P1_If_yes_line_2': otherStatesOfLiving && otherStatesOfLiving.size() > 1 ? otherStatesOfLiving[1..-1].join(', ') : '',
+  'Have you lived in a state other than California within the last five years?  yes': yesValue(jsonMap.lived_in_other_state),
+  'Have you lived in a state other than California within the last five years?  no': noValue(jsonMap.lived_in_other_state),
+  'If Yes Line 1': otherStatesOfLiving ? otherStatesOfLiving[0] : '',
+  'If Yes Line 2': otherStatesOfLiving && otherStatesOfLiving.size() > 1 ? otherStatesOfLiving[1..-1].join(', ') : '',
 
-  'P1_County': jsonMap.application_county?.value,
-  'P1_Date': jsonMap.application_date,
-  'P1_Name_of_resource_family': jsonMap.resource_family_name,
-  'P1_Residence_address_street_city_zip': formatAddress(getAddressStreetCityStateZip(jsonMap.residence_address)),
+  'COUNTY': jsonMap.application_county?.value,
+  'DATE': dateIsoToUs(jsonMap.application_date),
+  'NAME OF RESOURCE FAMILY': jsonMap.resource_family_name,
+  'RESIDENCE ADDRESS STREET CITY ZIP': formatAddress(getAddressStreetCityStateZip(jsonMap.residence_address)),
 
-  'P1_Date_of_birth': dateIsoToUs(jsonMap.date_of_birth),
-  'P1_Drivers_license_number/state': jsonMap.driver_license +'/'+ jsonMap.driver_license_state?.id,
+  'Date Of Birth': dateIsoToUs(jsonMap.date_of_birth),
+  'Drivers License Number/State': jsonMap.driver_license +'/'+ jsonMap.driver_license_state?.id,
+  'SOCIAL SECURITY NUMBER SEE PRIVACY STATEMENT': jsonMap.ssn,
+  'YOUR FULL NAME PRINT CLEARLY': getFullName(jsonMap),
 
-  'P1_Social_security_number_see_privacy_statement': jsonMap.ssn,
+  'DATE_2': dateIsoToUs(jsonMap.application_date),
+  'In which state and city did you commit the offense': [jsonMap.disclosures[0]?.offense_city, jsonMap.disclosures[0]?.offense_state?.value].join(', '),
+  'When did this happen': dateIsoToUs(jsonMap.disclosures[0]?.offense_date),
 
-  'P1_Your_full_name_print_clearly': getFullName(jsonMap),
-
-  'P2_Date_2': jsonMap.application_date,
-  'P2_In_which_state_and_city_did_you_commit_the_offense': [jsonMap.disclosures[0]?.offense_city, jsonMap.disclosures[0]?.offense_state?.value].join(', '),
-  'P2_When_did_this_happen': jsonMap.disclosures[0]?.offense_date,
-
-  'P2_What_was_the_offense_line_1': jsonMap.disclosures[0]?.offense,
-  'P2_Explain_what_happened_line_1': jsonMap.disclosures[0]?.offense_details
+  'What was the offense line 1': jsonMap.disclosures[0]?.offense,
+  'Explain what happened line 1': jsonMap.disclosures[0]?.offense_details
 ]
