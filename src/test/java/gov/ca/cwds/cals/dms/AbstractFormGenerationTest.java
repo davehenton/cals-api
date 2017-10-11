0b50c5cbab57a76bb23680e19dd69741f1a550f6
Sergii Redchuk <serge.redchuk@engagepoint.com>
2017-10-11 15:14:46 -0700

    #151729873: DMS: implement shared PDF generation support library in DMS

--- src/test/java/gov/ca/cwds/cals/dms/AbstractFormGenerationTest.java
+++ src/test/java/gov/ca/cwds/cals/dms/AbstractFormGenerationTest.java
@@ -1,66 +1,51 @@
 package gov.ca.cwds.cals.dms;
 
-import gov.ca.cwds.dms.services.PdfService;
-import groovy.json.JsonSlurper;
-import groovy.lang.Binding;
-import groovy.lang.GroovyShell;
+import gov.ca.cwds.dms.services.pdf.PdfServiceTest;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Map;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+import java.util.Map.Entry;
+import org.apache.commons.io.FileUtils;
 import org.junit.BeforeClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static io.dropwizard.testing.FixtureHelpers.fixture;
-
 /**
  * @author TPT-2 team
  */
-public abstract class AbstractFormGenerationTest {
+public abstract class AbstractFormGenerationTest extends PdfServiceTest {
+
   private static final Logger LOGGER = LoggerFactory.getLogger(AbstractFormGenerationTest.class);
 
   private static DateTimeFormatter dateTimeFormatter;
-  private static PdfService pdfService;
 
   @BeforeClass
   public static void setUpClass() {
+    PdfServiceTest.setUpClass();
     dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
-    pdfService = new PdfService();
   }
 
-  void generatePdf(String templatePath, String scriptPath, String request) throws Exception {
-    /*
-     * map json
-     */
-    Binding binding = new Binding();
-    binding.setVariable("jsonMap", new JsonSlurper().parseText(request));
-    GroovyShell shell = new GroovyShell(binding);
-    Map<String, String> outputMap = (Map<String, String>) shell.evaluate(fixture(scriptPath));
+  void generateAndAssertPdf(String pdfTemplatePath, String groovyMapping, String jsonData)
+      throws IOException {
 
-    /*
-     * generate PDF and save to file
-     */
-    String templateFileName = templatePath.replaceAll("^.*/|\\.pdf$", "");
-    String outputFileName = String.format("%s_%s.pdf", templateFileName, dateTimeFormatter.format(LocalDateTime.now()));
+    // calculate output file name
+    String templateFileName = pdfTemplatePath.replaceAll("^.*/|\\.pdf$", "");
+    String outputFileName = String
+        .format("%s_%s.pdf", templateFileName, dateTimeFormatter.format(LocalDateTime.now()));
 
+    // generate PDF to file
     FileOutputStream pdfOutputStream = new FileOutputStream(new File(outputFileName));
-    pdfService.generatePdf(getClass().getResourceAsStream("/" + templatePath), outputMap, pdfOutputStream);
-
+    Map<String, String> expectedFieldsMap = generatePdf(pdfTemplatePath, pdfOutputStream,
+        groovyMapping, jsonData);
     LOGGER.info("File is generated: {}", outputFileName);
 
-    /*
-     * read the generated PDF file and log its fields
-     */
-    PDDocument generatedPdf = PDDocument.load(new File(outputFileName));
-    List<PDField> fieldList = pdfService.getFields(generatedPdf);
-    for (PDField field : fieldList) {
-      LOGGER.info("placeholder = '{}', value = {}", field.getFullyQualifiedName(), field.getValueAsString());
+    assertGeneratedPdf(expectedFieldsMap, FileUtils.readFileToByteArray(new File(outputFileName)));
+
+    for (Entry<String, String> entry : expectedFieldsMap.entrySet()) {
+      LOGGER.info("placeholder = '{}', value = {}", entry.getKey(), entry.getValue());
     }
-    generatedPdf.close();
   }
 }
