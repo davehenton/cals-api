e381311ceed9d12aaae757f28f102a60c3e26d33
Alexander Serbin <alexander.serbin@engagepoint.com>
2017-11-28 19:55:55 -0800

    TPT2-38 added ponetic searc keywords creation in SubstituteCareProviderService

--- src/main/java/gov/ca/cwds/cals/service/FacilityService.java
+++ src/main/java/gov/ca/cwds/cals/service/FacilityService.java
@@ -8,7 +8,6 @@ import static javax.ws.rs.core.Response.Status.EXPECTATION_FAILED;
 
 import com.google.inject.Inject;
 import gov.ca.cwds.cals.Constants;
-import gov.ca.cwds.cals.Constants.PhoneticSearchTables;
 import gov.ca.cwds.cals.Utils;
 import gov.ca.cwds.cals.Utils.Applicant;
 import gov.ca.cwds.cals.Utils.StaffPerson;
@@ -69,7 +68,6 @@ import gov.ca.cwds.data.legacy.cms.dao.CountiesDao;
 import gov.ca.cwds.data.legacy.cms.dao.FacilityTypeDao;
 import gov.ca.cwds.data.legacy.cms.dao.LicenseStatusDao;
 import gov.ca.cwds.data.legacy.cms.dao.PlacementHomeDao;
-import gov.ca.cwds.data.legacy.cms.dao.SsaName3ParameterObject;
 import gov.ca.cwds.data.legacy.cms.dao.StateDao;
 import gov.ca.cwds.data.legacy.cms.entity.BaseCountyLicenseCase;
 import gov.ca.cwds.data.legacy.cms.entity.BasePlacementHome;
@@ -91,7 +89,6 @@ import java.io.Serializable;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.util.Collections;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -464,8 +461,6 @@ public class FacilityService implements CrudsService {
           state -> outOfStateCheckMapper.toOutOfStateCheck(substituteCareProvider, state),
           applicant.getRfa1bForm());
 
-      prepareSubstituteCareProviderPhoneticSearchKeywords(substituteCareProvider,
-          placementHome);
     }
 
     storeOtherChildren(rfaApplicantIdsMap, form, placementHome);
@@ -504,25 +499,6 @@ public class FacilityService implements CrudsService {
     return substituteCareProviderService.create(
         mapRFAEntitiesToSCP(form, applicant), parameterObject);
 
-  }
-
-  private void prepareSubstituteCareProviderPhoneticSearchKeywords(
-      SubstituteCareProvider substituteCareProvider,
-      PlacementHome placementHome) {
-    SsaName3ParameterObject parameterObject = new SsaName3ParameterObject();
-    parameterObject.setTableName(PhoneticSearchTables.SCP_PHTT);
-    parameterObject.setCrudOper("I");
-    parameterObject.setIdentifier(substituteCareProvider.getIdentifier());
-    parameterObject.setFirstName(substituteCareProvider.getFirstNm());
-    parameterObject.setMiddleName(substituteCareProvider.getMidIniNm());
-    parameterObject.setLastName(substituteCareProvider.getLastNm());
-    parameterObject.setStreetNumber(placementHome.getStreetNo());
-    parameterObject.setStreetName(placementHome.getStreetNm());
-    parameterObject.setGvrEntc((short) 0);
-    parameterObject.setUpdateTimeStamp(new Date());
-    parameterObject.setUpdateId(substituteCareProvider.getLstUpdId());
-
-    ssaName3Dao.callStoredProc(parameterObject);
   }
 
   private PlacementHome createPlacementHomeInCWSCMS(RFA1aFormDTO form,
