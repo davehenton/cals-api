a23165a205deb4a6e18a723a117bee2218ad3211
Vladimir Petrusha <vladimr@petrusha.net>
2017-12-06 17:10:25 -0800

    TPT2-644 Add additional methods to 198b API

--- src/main/java/gov/ca/cwds/cals/service/rfa/LIC198bService.java
+++ src/main/java/gov/ca/cwds/cals/service/rfa/LIC198bService.java
@@ -2,17 +2,31 @@ package gov.ca.cwds.cals.service.rfa;
 
 import com.google.inject.Inject;
 import gov.ca.cwds.cals.persistence.dao.calsns.LIC198bDao;
+import gov.ca.cwds.cals.persistence.dao.calsns.RFA1aApplicantDao;
+import gov.ca.cwds.cals.persistence.dao.calsns.RFA1aOtherAdultDao;
 import gov.ca.cwds.cals.persistence.model.calsns.rfa.LIC198bForm;
+import gov.ca.cwds.cals.persistence.model.calsns.rfa.RFA1aApplicant;
+import gov.ca.cwds.cals.persistence.model.calsns.rfa.RFA1aOtherAdult;
+import gov.ca.cwds.cals.service.dto.rfa.RFA1bFormDTO;
 import gov.ca.cwds.cals.service.dto.rfa.lic198b.LIC198bFormDTO;
 import gov.ca.cwds.cals.service.rfa.factory.LIC198bFactory;
+import gov.ca.cwds.cals.web.rest.parameter.RFAApplicantAwareEntityGetParameterObject;
 import gov.ca.cwds.cals.web.rest.parameter.RFAApplicantAwareEntityUpdateParams;
+import gov.ca.cwds.cals.web.rest.parameter.RFAExternalEntityGetParameterObject;
 import gov.ca.cwds.cals.web.rest.parameter.RFAExternalEntityUpdateParameterObject;
+import gov.ca.cwds.cals.web.rest.parameter.RFAOtherAdultAwareEntityGetParameterObject;
 import gov.ca.cwds.cals.web.rest.parameter.RFAOtherAdultAwareEntityUpdateParams;
 
 /**
  * @author CWDS CALS API Team
  */
 public class LIC198bService extends AbstractRFAExternalEntityService<LIC198bForm, LIC198bFormDTO> {
+
+  @Inject
+  private RFA1aApplicantDao applicantDao;
+
+  @Inject
+  private RFA1aOtherAdultDao otherAdultDao;
 
   @Inject
   public LIC198bService(LIC198bDao dao) {
@@ -35,6 +49,32 @@ public class LIC198bService extends AbstractRFAExternalEntityService<LIC198bForm
 
     }
     return extractDTO(lic198bForm);
+  }
+
+  @Override
+  public LIC198bFormDTO find(RFAExternalEntityGetParameterObject params) {
+    Long entityId = params.getEntityId();
+    if (params instanceof RFAApplicantAwareEntityGetParameterObject) {
+      RFA1aApplicant applicant = applicantDao.find(entityId);
+      return extractDTO(applicant.getLic198bForm());
+    } else if (params instanceof RFAOtherAdultAwareEntityGetParameterObject) {
+      RFA1aOtherAdult rfa1aOtherAdult = otherAdultDao.find(entityId);
+      return extractDTO(rfa1aOtherAdult.getLic198bForm());
+    }
+    return super.find(params);
+  }
+
+  public LIC198bFormDTO find(RFAExternalEntityUpdateParameterObject<RFA1bFormDTO> params) {
+    if (params instanceof RFAApplicantAwareEntityUpdateParams) {
+      Long applicantId = ((RFAApplicantAwareEntityUpdateParams) params).getApplicantId();
+      RFA1aApplicant applicant = applicantDao.find(applicantId);
+      return extractDTO(applicant.getLic198bForm());
+    } else if (params instanceof RFAOtherAdultAwareEntityUpdateParams) {
+      Long otherAdultId = ((RFAOtherAdultAwareEntityUpdateParams) params).getOtherAdultId();
+      RFA1aOtherAdult rfa1aOtherAdult = otherAdultDao.find(otherAdultId);
+      return extractDTO(rfa1aOtherAdult.getLic198bForm());
+    }
+    return null;
   }
 
 
