e298970887806ce1204e00b14370c7b6df29374e
Alexander Serbin <alexander.serbin@engagepoint.com>
2017-12-12 17:11:43 -0800

    TPT2-112 merging with develop

--- src/main/java/gov/ca/cwds/cals/service/rfa/RFA1aFormService.java
+++ src/main/java/gov/ca/cwds/cals/service/rfa/RFA1aFormService.java
@@ -211,7 +211,8 @@ public class RFA1aFormService
           }
         });
 
-    Set<IssueDetails> detailsList = droolsService.performBusinessRules(formDTO, createConfiguration());
+    Set<IssueDetails> detailsList = droolsService
+        .performBusinessRules(formDTO, createConfiguration());
     if (!detailsList.isEmpty()) {
       throw new BusinessValidationException(detailsList);
     }
