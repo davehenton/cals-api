4bbe184508e366003decd4d2d1e003246e9720d8
Ievgenii Drozd <justajavaboy@users.noreply.github.com>
2018-01-11 13:34:00 -0800

    CAL1-57 Facility search issue in Integration fixed phone mapper delegate for LIS

--- src/main/java/gov/ca/cwds/cals/service/mapper/PhoneMapperDecorator.java
+++ src/main/java/gov/ca/cwds/cals/service/mapper/PhoneMapperDecorator.java
@@ -1,5 +1,6 @@
 package gov.ca.cwds.cals.service.mapper;
 
+import gov.ca.cwds.cals.persistence.model.lisfas.LisFacFile;
 import gov.ca.cwds.cals.service.dto.PersonPhoneDTO;
 import gov.ca.cwds.data.legacy.cms.entity.BasePlacementHome;
 import org.apache.commons.lang3.StringUtils;
@@ -9,27 +10,39 @@ import org.apache.commons.lang3.StringUtils;
  */
 public abstract class PhoneMapperDecorator implements PhoneMapper {
 
-    private PhoneMapper delegate;
+  private PhoneMapper delegate;
 
-    public PhoneMapperDecorator(PhoneMapper delegate) {
-        this.delegate = delegate;
-    }
+  public PhoneMapperDecorator(PhoneMapper delegate) {
+    this.delegate = delegate;
+  }
 
-    @Override
-    public PersonPhoneDTO toPrimaryPhoneDTO(BasePlacementHome placementHome) {
-      PersonPhoneDTO personPhoneDTO = null;
-        if (StringUtils.isNotBlank(placementHome.getPrmTelNo())) {
-          personPhoneDTO = delegate.toPrimaryPhoneDTO(placementHome);
-        }
-      return personPhoneDTO;
+  @Override
+  public PersonPhoneDTO lisFacilityToPhoneDTO(LisFacFile lisFacFile) {
+    PersonPhoneDTO personPhoneDTO = null;
+    if (StringUtils.isNotBlank(lisFacFile.getFacPhoneNbr()) && !lisFacFile.getFacPhoneNbr()
+        .equalsIgnoreCase("null")) {
+      personPhoneDTO = delegate.lisFacilityToPhoneDTO(lisFacFile);
     }
+    return personPhoneDTO;
+  }
 
-    @Override
-    public PersonPhoneDTO toAlternatePhoneDTO(BasePlacementHome placementHome) {
-      PersonPhoneDTO personPhoneDTO = null;
-        if (StringUtils.isNotBlank(placementHome.getBckTelNo())) {
-          personPhoneDTO = delegate.toAlternatePhoneDTO(placementHome);
-        }
-      return personPhoneDTO;
+  @Override
+  public PersonPhoneDTO toPrimaryPhoneDTO(BasePlacementHome placementHome) {
+    PersonPhoneDTO personPhoneDTO = null;
+    if (StringUtils.isNotBlank(placementHome.getPrmTelNo()) && !placementHome.getPrmTelNo()
+        .equalsIgnoreCase("null")) {
+      personPhoneDTO = delegate.toPrimaryPhoneDTO(placementHome);
     }
+    return personPhoneDTO;
+  }
+
+  @Override
+  public PersonPhoneDTO toAlternatePhoneDTO(BasePlacementHome placementHome) {
+    PersonPhoneDTO personPhoneDTO = null;
+    if (StringUtils.isNotBlank(placementHome.getBckTelNo()) && !placementHome.getBckTelNo()
+        .equalsIgnoreCase("null")) {
+      personPhoneDTO = delegate.toAlternatePhoneDTO(placementHome);
+    }
+    return personPhoneDTO;
+  }
 }
