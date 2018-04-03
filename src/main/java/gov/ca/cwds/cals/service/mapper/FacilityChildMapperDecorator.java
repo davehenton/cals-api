04278f5e23f883df03d28e38cc031304b23ff5aa
Ievgenii Drozd <justajavaboy@users.noreply.github.com>
2018-04-03 05:16:19 -0700

    Added decorator for FacilityChildMapper

---
+++ src/main/java/gov/ca/cwds/cals/service/mapper/FacilityChildMapperDecorator.java
@@ -0,0 +1,25 @@
+package gov.ca.cwds.cals.service.mapper;
+
+import gov.ca.cwds.cals.service.dto.FacilityChildDTO;
+import gov.ca.cwds.data.legacy.cms.entity.Client;
+
+/**
+ * @author CWDS CALS API Team
+ */
+public abstract class FacilityChildMapperDecorator implements FacilityChildMapper {
+
+  private FacilityChildMapper delegate;
+
+  public FacilityChildMapperDecorator(FacilityChildMapper delegate) {
+    this.delegate = delegate;
+  }
+
+  @Override
+  public FacilityChildDTO toFacilityChildDTO(Client client) {
+    FacilityChildDTO facilityChildDTO = null;
+    if (null != client) {
+      facilityChildDTO = delegate.toFacilityChildDTO(client);
+    }
+    return facilityChildDTO;
+  }
+}
