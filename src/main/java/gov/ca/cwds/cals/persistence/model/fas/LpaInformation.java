975f933bfcb89e15db00f4258ad5aa3911e0c1f5
Leonid Marushevskyi <leonid.marushevskiy@osi.ca.gov>
2018-04-03 22:51:17 -0700

    CALS-4319: codeclimate fixes

--- src/main/java/gov/ca/cwds/cals/persistence/model/fas/LpaInformation.java
+++ src/main/java/gov/ca/cwds/cals/persistence/model/fas/LpaInformation.java
@@ -19,8 +19,8 @@ import java.time.LocalDateTime;
  */
 @NamedQuery(
     name = LpaInformation.FIND_ASSIGNED_WORKER_BY_LPA_CODE,
-    query = "SELECT l FROM LpaInformation l WHERE SUBSTRING(l.lpaCode, 3) = :" +
-        LpaInformation.PARAM_LPA_CODE
+    query = "SELECT l FROM LpaInformation l WHERE SUBSTRING(l.lpaCode, 3) = :"
+        + LpaInformation.PARAM_LPA_CODE
 )
 @Entity
 @Table(name = "lpa_information")
