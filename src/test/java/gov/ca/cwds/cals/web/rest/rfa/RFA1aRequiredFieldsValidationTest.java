498ab8cf3714266fa563cbec1f67bc542f9b1a74
Leonid Marushevskyi <leonid.marushevskiy@osi.ca.gov>
2018-05-07 13:09:00 -0700

    Revert "Revert "CALS-5990 Change backend API for RFA Minor child""
    This reverts commit ca978e8

--- src/test/java/gov/ca/cwds/cals/web/rest/rfa/RFA1aRequiredFieldsValidationTest.java
+++ src/test/java/gov/ca/cwds/cals/web/rest/rfa/RFA1aRequiredFieldsValidationTest.java
@@ -259,8 +259,8 @@ public class RFA1aRequiredFieldsValidationTest extends BaseRFAIntegrationTest {
     MinorChildDTO minorChild = minorChildHelper.buildNewMinorChildDTO(firstApplicant);
     minorChild.setDateOfBirth(null);
     minorChild.setGender(null);
-    minorChild.setChildFinanciallySupported(null);
-    minorChild.setChildAdopted(null);
+    minorChild.getRelationshipToApplicants().get(0).setChildFinanciallySupported(null);
+    minorChild.getRelationshipToApplicants().get(0).setChildAdopted(null);
     return minorChild;
   }
 
